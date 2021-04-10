af9b4884ea7bfe7477b8e2864c8469d0fc5fe960
Lars Mühlenstädt <muehlenstaedt.lars@gmail.com>
2021-04-10 13:34:28 +0200

    remove folder

--- setup_requirements.sh
+++ setup_requirements.sh
@@ -2,6 +2,8 @@ sudo apt-get update
 
 sudo apt-get install chromium-browser unclutter build-essential docker docker-compose -y
 
+rm -Rf server
+
 git clone https://github.com/Pi-Hardware-Monitor/server.git
 
 cd server
@@ -9,6 +11,8 @@ cd server
 git checkout mvp
 
 cd ..
+
+rm -Rf display
 
 git clone https://github.com/Pi-Hardware-Monitor/display.git
 
